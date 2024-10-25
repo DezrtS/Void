fb866886334c03551b1a52df66af62560cfb9269
BezerkMangos <Sattaur04@gmail.com>
2024-10-24 20:03:34 -0400

    Command Pattern

---
+++ Void/Assets/Scenes/Aatif - Test/ComputerTaskTest/ComputerCommandTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ComputerCommandTest 
+{
+    public abstract void Execute();
+
+    public abstract void Undo();
+
+
+}
