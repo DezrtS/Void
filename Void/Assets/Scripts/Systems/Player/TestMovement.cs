466f8412ea69fb6c87d138c650aa5e68bffec446
MikeyA-22 <subomi.atteh@ontariotechu.net>
2024-11-17 17:57:55 -0500

    updated and added aiming-IK

--- Void/Assets/Scripts/Systems/Player/TestMovement.cs
+++ Void/Assets/Scripts/Systems/Player/TestMovement.cs
@@ -7,17 +7,28 @@ public class TestMovement : MonoBehaviour
 {
     private Rigidbody rig;
     [SerializeField] private float speed;
+    Animator animator;
+    [SerializeField] float movementmultiplier = 1.2f;
+    
 
     private void Awake()
     {
         rig = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
+
+        
+        animator.SetFloat("xinput", (float)(input.x * movementmultiplier));
+        animator.SetFloat("yinput", (float)(input.z * movementmultiplier));
+        
         rig.velocity = speed * input.normalized;
     }
 }
